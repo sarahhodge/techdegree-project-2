bb9fc9a1f29e295ebcaa6492963269df2d4794a2
Sarah <smkb17@gmail.com>
2020-03-10 10:20:47 -0500

    click event and calling the functions
    Page is working correctly to meet expectations

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -6,76 +6,57 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
+//global variables to store the students and items per page
 const listItems = document.querySelectorAll('.student-item');
 const itemsPerPage = 10;
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
 
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
-
+/* The showPage() will only show 10 students on each page by hiding
+   all other listItems
+*/
 const showPage = (listItems, pageNum) => {
   let startIndex = (pageNum * itemsPerPage) - itemsPerPage
   let endIndex = pageNum * itemsPerPage
     for(let i = 0; i < listItems.length; i += 1){
         if(i >= startIndex && i < endIndex) {
-         listItems[i].style.display= 'block';
+         listItems[i].style.display= '';
       } else {
          listItems[i].style.display = 'none';
-        }
-    }
-}
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
+      };
+   };
+};
+/*
+The appendPageLinks() creates the div and ul. Then loops over and creates
+the li and a for each page with 10 items per page
+*/
 const appendPageLinks = (listItems) => {
-  const pageNum= listItems.length/itemsPerPage;
+  const pageTotal= Math.ceil(listItems.length/itemsPerPage);
   const main = document.querySelector('.page');
-  const divPagination = document.createElement('div');
-      divPagination.className= 'pagination';
-      main.appendChild(divPagination);
+  const div = document.createElement('div');
+      div.className= 'pagination';
+      main.appendChild(div);
+  const ul= document.createElement('ul');
+      div.appendChild(ul);
 
-  const ulPagination= document.createElement('ul');
-      divPagination.appendChild(ulPagination);
+  for (let i = 0; i < pageTotal; i +=1){
+      const li = document.createElement('li');
+      const a= document.createElement('a');
+      if (i === 0){
+        a.className = 'active'};
+      li.appendChild(a);
+      ul.appendChild(li);
+      a.href = '#';
+      a.textContent = i+1;
+// If the user clicks a page number, then the a.className is set to active
+      a.addEventListener('click', (e) => {
+      let active = document.querySelector('a.active');
+      active.className = ''
+      e.target.className = 'active';
+      showPage(listItems, e.target.textContent);
+    });
+  };
+};
 
-  for (let i = 0; i < pageNum; i +=1){
-    const li = document.createElement('li');
-    const a= document.createElement('a');
-    a.href = '#';
-    a.text = i+1;
-
-    li.appendChild(a);
-    ulPagination.appendChild(li);
-  }
-}
+//calling the functions
+showPage(listItems, 1);
 appendPageLinks(listItems);
-
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
